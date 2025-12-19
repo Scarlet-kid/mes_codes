589223cb2d6163b12b8eb721d83c94fc7146cc6e
Scarlet-kid <sosthenetoviekou@gmail.com>
2025-12-19 10:24:42 +0100

    reduc algo trie

--- train_TPcontrol_python/Tri_insertion.py
+++ train_TPcontrol_python/Tri_insertion.py
@@ -9,14 +9,3 @@ def triInsertion(lst:list)->None:
             lst[j] = lst[j-1]
             j = j - 1
         lst[j] = tmp
-        
-"""        
-def triInsertion(lst: list) -> None:
-    for i in range(1, len(lst)):
-        tmp = lst[i]
-        j = i
-        while j > 0 and lst[j-1] > tmp:
-            lst[j] = lst[j-1]
-            j -= 1
-        lst[j] = tmp
-"""
