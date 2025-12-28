7885744ff79766e8f906b6e09b1c8e782dbd80bc
Sosthene <sosthenetoviekou@gmail.com>
2025-12-28 23:15:33 +0100

    new puissance

--- Code_Perso_Cours_Polet/assert.py
+++ Code_Perso_Cours_Polet/assert.py
@@ -6,3 +6,11 @@ def puissance(x:float,n:int)->float:
         return x * puissance(x,n-1)
 
 print(puissance(3,2.5))
+
+def puissance(x,n):
+    res = 1
+    for i in range(n):
+        res *= x
+    return res
+
+print(puissance(2,1))
