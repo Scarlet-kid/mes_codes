49ed45b4e9b94dee4ea3b8af84dc4d30bfec3aea
Sosthene <sosthenetoviekou@gmail.com>
2026-01-01 22:40:50 +0100

    mise a jour

--- Code_Perso_Cours_Polet/lst_cmphs.py
+++ Code_Perso_Cours_Polet/lst_cmphs.py
@@ -1,5 +1,21 @@
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     maListe = [x**3 for x in [y for y in range(1,11) if y%2==0] if (x**3)%3==0]
     #print(maListe)
     maListe = [(x, y, x and y) for x in [True, False] for y in [True, False]]
-    print(maListe)
+    print(maListe)"""
+
+def f(x, y):
+    if x==y:
+        return 1
+    else:
+        return 0
+
+def identity(taille):
+    return [[f(l,c) for l in range(taille)]for c in range(taille)]
+
+def transpose(m):
+    return [[ligne[i] for ligne in m] for i in range(len(m[0]))]
+
+if __name__=="__main__":
+    i = identity(3)
+    print(i)
