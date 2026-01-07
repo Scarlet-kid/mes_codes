702b628b458269e8ef6379229a7ed7d923d83718
Scarlet-kid <sosthenetoviekou@gmail.com>
2026-01-07 22:41:51 +0100

    modif algo trie

--- train_TPcontrol_python/Tri_insertion.py
+++ train_TPcontrol_python/Tri_insertion.py
@@ -1,8 +1,12 @@
-def triInsertion(lst: list) -> None:
-    for i in range(1, len(lst)):
+def triParInsertion(lst:list) -> None:
+    for i in range(1,len(lst)):
         tmp = lst[i]
         j = i
-        while j > 0 and lst[j - 1] > tmp:
-            lst[j] = lst[j - 1]
+        while j>0 and lst[j-1] > tmp:
+            lst[j] = lst[j-1]
             j -= 1
         lst[j] = tmp
+
+MaListe = [12,5,3,4,10,11]
+triParInsertion(MaListe)
+print(MaListe)
