b55b5f47447ef14e9a9dbed530d5e2b82ed09fe7
Sosthene <sosthenetoviekou@gmail.com>
2026-01-22 23:48:26 +0100

    rename TD fiche1 To TDfiche1

---
+++ LanguageC_début/TDfiche1/exo3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void saisieBorneeIter(int mini, int maxi, int *value)
+{
+  int fini = 0;
+  int res;
+  while(!fini)
+  {
+    printf("Saisir une valeur entre %d et %d:",mini,maxi);
+    scanf("%d",&res);
+    if (res<mini || res>maxi)
+    {
+      printf("Saisie incorrect");
+    }
+    else
+    {
+      *value = res;
+      fini = 1;
+    }
+  }
+}
