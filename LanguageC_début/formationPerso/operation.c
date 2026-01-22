dfa6331575de36a178111d3d956cff2ac381ae02
Sosthene <sosthenetoviekou@gmail.com>
2026-01-22 20:24:19 +0100

    un prog qui calcul deux sommes

---
+++ LanguageC_début/formationPerso/operation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char op ;
+    int n1, n2 ;
+    printf("opération souhaitée (+ ou *) ? ");
+    scanf("%c",&op) ;
+    printf("Donnez 2 nombres entiers : ");
+    scanf("%d %d",&n1,&n2);
+    if (op == '+')
+    {
+        printf ("leur somme est %d",n1+n2);
+    } 
+    else
+    {
+        printf("leur produit est : %d\n",n1*n2);
+    }
+    return 0;
+}
