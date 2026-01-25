7eb5da484d665adf991f06b63656487e0fe1edab
Sosthene <sosthenetoviekou@gmail.com>
2026-01-25 19:15:10 +0100

    finalisation exo3 fiche de TD1 (prog version Itérative + Récursive) et prog principal

--- LanguageC_début/TDfiche1/exo3.c
+++ LanguageC_début/TDfiche1/exo3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int verifie(int val1,int val2)
+int verifie_iterative(int val1,int val2)
 {
   _Bool fini = 0;
   int val;
@@ -20,7 +20,35 @@ int verifie(int val1,int val2)
   return val;
 }
 
+int verifie_recurssive(int val1,int val2)
+{
+  int res;
+  printf("Saisir une valeur entre %d et %d :",val1,val2);
+  scanf("%d",&res);
+  if(res<val1 || res>val2)
+  {
+    printf("Saisie incorrecte\n");
+    return verifie_recurssive(val1, val2);
+  }
+
+  else
+  {
+      return res;
+  }
+}
+
+
 int main(){
-  printf("%d\n",verifie(1,10));
+  int choix;
+  printf("Quelle version voulez-vous tester ?(1 -> itérative et 2 -> recurssive):");
+  scanf("%d",&choix);
+  if (choix == 1)
+  {
+    printf("%d\n",verifie_iterative(1,10));
+  }
+  else
+  {
+    printf("%d\n",verifie_recurssive(1,10));
+  }
   return 0;
 }
