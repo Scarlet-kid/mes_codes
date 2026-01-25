7e9e3c4ce6973ab61b479590188f28536284a9f1
Sosthene <sosthenetoviekou@gmail.com>
2026-01-25 19:43:56 +0100

    finalisation du TD5 et du TDfiche1

--- LanguageC_début/TDfiche1/exo5.c
+++ LanguageC_début/TDfiche1/exo5.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-void afficher_entete(){
+
+int main()
+{
+  int i, j;
   printf("X\t");
-  int i;
-  for (i=0;i<=10;i++)
+  for (j=0; j<=10; j++)
   {
-    printf("%i",i);
+    printf("%d\t",j);
   }
-}
-
-
-void afficher(){
-
+  printf("\n");
+  for(i=0; i<=10; i++)
+  {
+    printf("%d\t",i);
+    for ( j = 0; j <= 10; j++)
+    {
+      printf("%d\t", i*j);
+    }
+    printf("\n");
+  }
+  return 0;
 }
