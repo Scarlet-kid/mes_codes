7ca77e5dc3b9ca68fec8843a3da384641d8a18c0
Sosthene <sosthenetoviekou@gmail.com>
2026-02-04 21:04:44 +0100

    fix bug TD2.py

--- R2.08/TP1/TD2.py
+++ R2.08/TP1/TD2.py
@@ -1,17 +1,17 @@
-from code import *
+import code
 
-print("moyenne de x :",moyenne([i for i in range(5,31,5)]))
-print("moyenne de y :",moyenne([84,58,30,19,7,4]))
-print("ecart-type de x:",ecart([i for i in range(5,31,5)]))
-print("ecart-type de y:",ecart([84,58,30,19,7,4]))
+print("moyenne de x :",code.moyenne([i for i in range(5,31,5)]))
+print("moyenne de y :",code.moyenne([84,58,30,19,7,4]))
+print("ecart-type de x:",code.ecart([i for i in range(5,31,5)]))
+print("ecart-type de y:",code.ecart([84,58,30,19,7,4]))
 
 def covariance(x:list[float],y:list[float]):
-    maLong = long(x)
-    moyX = moyenne(x)
-    moyY = moyenne(y)
+    maLong = code.long(x)
+    moyX = code.moyenne(x)
+    moyY = code.moyenne(y)
 
     res = 0
-    for i in range(long(x)):
+    for i in range(code.long(x)):
         res+=(x[i]*y[i])
     
     return (res/maLong) - (moyX*moyY)
@@ -19,7 +19,7 @@ def covariance(x:list[float],y:list[float]):
 print("La covariance est :",covariance([i for i in range(5,31,5)],[84,58,30,19,7,4]))
 
 def coefdir(x:list[float],y:list[float])->float:
-    return covariance(x,y)/variance(x)
+    return covariance(x,y)/code.variance(x)
 
 print("Le coefficient directeur est :",coefdir([i for i in range(5,31,5)],[84,58,30,19,7,4]))
 
