bf06ed4c3fb92baddd0b8024f1a3f9dfcc99c21c
Sosthène <sosthenetoviekou@gmail.com>
2026-02-13 12:40:12 +0100

    TP3-C terminé en grande partie

--- LanguageC/TP3/exo2.c
+++ LanguageC/TP3/exo2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 typedef struct
 {
@@ -24,21 +26,152 @@ void afficher_date(Date d)
 typedef struct 
 {
     char* nom;
-    char* prenom;
+    char* prenom; 
     Date naissance;
 }Personne;
 
-Personne CreerPersonne(char*Monnom, char*Monprenom, Date Manaissance)
+Personne CreerPersonne(char* Monnom, char *Monprenom, Date Manaissance)
 {
     Personne maPersonne;
     maPersonne.nom = Monnom;
     maPersonne.prenom = Monprenom;
     maPersonne.naissance = Manaissance;
+    return maPersonne;
 }
 
 void afficher_Personne(Personne p)
 {
-    printf("%s-%s-%s\n",p.nom,p.prenom,p.naissance);
+    printf("%s-%s ",p.nom,p.prenom);
+    afficher_date(p.naissance);
 }
 
+void ajouterPfile1(Personne p)
+{
+    FILE* pf;
+    pf  = fopen("exemple1.txt","w");
+    fprintf(pf,"Nom: %s, prénom : %s naissance %d-%d-%d",p.nom,p.prenom,p.naissance.jour,p.naissance.mois,p.naissance.annee);
+    fclose(pf);
+}
+
+void ajouterPfile2(Personne p)
+{
+    FILE* pf;
+    pf  = fopen("exemple1.dat","ab");
+    fwrite(&p,sizeof(Personne),1,pf);
+    fclose(pf);
+}
+
+
+
+
+void demanderUtl1()
+{
+    Personne per;
+    FILE* pf;
+    pf  = fopen("exemple2.txt","w");
+    int nb;
+    printf("Combien de personnes voulez-voud ajouter?:");
+    scanf("%d",&nb);
+    for(int i=0;i<nb;i++)
+    {
+        char name[50];
+        char surname[50];
+        int j;
+        int m;
+        int a;
+        printf("nom:");
+        scanf("%s",name);
+        printf("prenom:");
+        scanf("%s",surname);
+        printf("jour de naissance:");
+        scanf("%d",&j);
+        printf("mois de naissance:");
+        scanf("%d",&m);
+        printf("année de naissance:");
+        scanf("%d",&a);
+        fprintf(pf,"Nom: %s, prénom : %s naissance %d-%d-%d\n",per.nom=name,per.prenom=surname,per.naissance.jour=j,per.naissance.mois=m,per.naissance.annee=a);
+    }
+    fclose(pf);
+}
+
+void demanderUtl2()
+{
+    Personne per;
+    FILE* pf;
+    pf  = fopen("exemple2.dat","w");
+    int nb;
+    printf("Combien de personnes voulez-voud ajouter?:");
+    scanf("%d",&nb);
+    for(int i=0;i<nb;i++)
+    {
+        char name[50];
+        char surname[50];
+        int j;
+        int m;
+        int a;
+        printf("nom:");
+        scanf("%s",name);
+        printf("prenom:");
+        scanf("%s",surname);
+        printf("jour de naissance:");
+        scanf("%d",&j);
+        printf("mois de naissance:");
+        scanf("%d",&m);
+        printf("année de naissance:");
+        scanf("%d",&a);
+        fwrite(&per,sizeof(Personne),nb,pf);
+    }
+    fclose(pf);
+}
+
+void recupererEtAfficher10() {
+    FILE* pf = fopen("exemple2.txt", "r");
+    if (pf == NULL) {
+        printf("Erreur : Impossible d'ouvrir le fichier.\n");
+        return;
+    }
+
+    char nom[50], prenom[50];
+    int j, m, a;
+    int compteur = 0;
+
+    printf("\n--- Lecture du fichier (Format spécifique) ---\n");
+
+    while (compteur < 10 && fscanf(pf, "Nom: %[^,], prénom : %s naissance %d-%d-%d\n", 
+                                   nom, prenom, &j, &m, &a) == 5) {
+        
+        printf("Personne %d : %s %s né le %d/%d/%d\n", 
+               compteur + 1, nom, prenom, j, m, a);
+        compteur++;
+    }
+
+    if (compteur == 0) {
+        printf("Échec de la lecture. Vérifie que le fichier contient bien des lignes au format :\n");
+        printf("Nom: XXX, prénom : YYY naissance J-M-A\n");
+    }
+
+    fclose(pf);
+}
+
+
+int main()
+{
+    /*Date D;
+    D = CreerDate(13,2,2025);
+    afficher_date(D);
+
+    Personne P;
+    P = CreerPersonne("sosthene","eddy",D);
+    afficher_Personne(P);
+
+    ajouterPfile1(P);
+    ajouterPfile2(P);*/
+
+    //demanderUtl1();
+
+    //demanderUtl2();
+
+    recupererEtAfficher10();
+    return 0;
+}
 
